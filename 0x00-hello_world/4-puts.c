58446ed233e0a0d61d85d2615ce3565ed21af435
Marvin Kurland <bugemarvin@outlook.com>
2022-04-08 00:22:27 +0300

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main-Creating a C program that outputs by use of function puts
+ * Return: 0 if successful
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
