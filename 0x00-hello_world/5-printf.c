49b156ddde4206d47744d9e8b1fda21e6655835c
Marvin Kurland <bugemarvin@outlook.com>
2022-04-08 01:09:46 +0300

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+*Description: main-use of print a statement
+*Return: 0 for succes
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
