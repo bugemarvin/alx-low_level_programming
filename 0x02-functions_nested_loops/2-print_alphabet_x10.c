38a80e15964a013adf296e59ec19df8c664c680c
Marvin Kurland <bugemarvin@outlook.com>
2022-04-13 23:23:24 +0300

    task 2 x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,21 @@
 #include "main.h"
 /**
-*Description: main - 
+*print_alphabet_x10 - as function entry point
 *Return: 0 for succes
 */
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
-
-	while (i < 10)
-	{
-		c = 'a';
-		while (c <= 'z')
-		{
-			_putchar(c);
-			c++;
-		}
-		_putchar('\n');
-		i++;
-	}
-
+char c;
+int i = 0;
+while (i < 10)
+{
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+i++;
+}
 }
