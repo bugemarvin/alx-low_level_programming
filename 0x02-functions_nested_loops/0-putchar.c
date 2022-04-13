7983fc1e00c4e5ffea05c1893eecf0e181ebfb17
Marvin Kurland <bugemarvin@outlook.com>
2022-04-13 22:06:59 +0300

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *Description: main - 
  *Return: 0 for succes
  */
  int main(void){
-     
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
  }
