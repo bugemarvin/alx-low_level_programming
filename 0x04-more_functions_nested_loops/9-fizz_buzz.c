8392373877e944907f522e4b717a7a8beab3669b
Marvin Kurland <bugemarvin@outlook.com>
2022-04-15 00:28:59 +0300

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,17 @@ int a;
 for (a = 1; a <= 100; a++)
 {
 if ((a % 3) == 0)
-{
 printf("Fizz");
 if ((a % 5) == 0)
-{
 printf("Buzz");
 if ((a % 15) == 0)
-{
 printf("FizzBuzz");
-}
-}
-}
 else
-{
 printf("%d", a);
-}
+printf(" ");
+if (a == 100)
 printf(" ");
 }
+printf("\n);
 return (0);
 }
