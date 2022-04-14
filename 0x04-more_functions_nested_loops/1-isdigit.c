462d14d09af604b32de95158b26ec113b24a1500
Marvin Kurland <bugemarvin@outlook.com>
2022-04-14 22:40:36 +0300

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 /**
 *_isdigit - main enter block
 *Description: main - checking for a digit
-*@n: prints a number
+*@c: prints a number
 *Return: 0 or 1 Always succes
 *On error, -1 is returned, and errno is set appropriately
 */
-int _isdigit(int n)
+int _isdigit(int c)
 {
-if (n >= 0 && n <= 9)
+if (c >= 0 && c <= 9)
 {
 return (1);
 }
