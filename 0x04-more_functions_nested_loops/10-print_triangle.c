d26f204910a2646a8113521061644e72feb19f8c
Marvin Kurland <bugemarvin@outlook.com>
2022-04-19 00:56:07 +0300

    task 10 size of a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,20 +9,21 @@ void print_triangle(int size)
 int a;
 if (size > 0)
 {
-for (a = 0; a < size; a++)
-{
-for (b = 0; b < size; b++)
-{
-if (b >= (size + a))
-{
-    _putchar(' ');
+    for (a = 0; a < size; a++)
+        {
+            for (b = 0; b < size; b++)
+                {
+                if (b >= (size + a))
+                    {
+                        _putchar(' ');
+                    }
+                else
+                    {
+                    _putchar('#');
+                    }
+                }
+                _putchar('\n');
+        }
 }
-else
-{
-_putchar('#');
-}
-}
-}
-}
-_putchar('\n');
+    _putchar('\n');
 }
