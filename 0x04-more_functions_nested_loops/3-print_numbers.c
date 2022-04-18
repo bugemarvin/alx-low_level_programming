914b92f84a011a53fba0902df4ec47d091a87c48
Marvin Kurland <bugemarvin@outlook.com>
2022-04-18 14:05:17 +0300

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
- * print_numbers - Main entry point
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+*print_numbers - Main entry point.
+*Return: On success 1.
+*On error, -1 is returned, and errno is set appropriately.
+*/
 void print_numbers(void)
 {
+for (numbers = 0; numbers <= 9; numbers++)
+_putchar(numbers);
+_putchar('\n');
 return (0);
 }
