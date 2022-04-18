4ba4f3e1d2f50b24a43ef66322da4bcc7c92373b
Marvin Kurland <bugemarvin@outlook.com>
2022-04-19 01:13:56 +0300

    task 10 size of a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,33 @@
 #include "main.h"
+
 /**
- * print_triangle - Main entry point
+ * print_triangle - Main entry point.
+ * @size: this is the size of the trinangle.
  * Return: 0 or 1 On success.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 void print_triangle(int size)
 {
 if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int a; 
-        int b;
-
-		for (a = 1; a <= size; a++)
-		{
-			for (b = a; b < size; b++)
-			{
-				_putchar(' ');
-			}
-
-			for (b = 1; b <= a; b++)
-			{
-				_putchar('#');
-			}
-
-			_putchar('\n');
-		}
-	}
+{
+_putchar('\n');
+}
+else
+{
+int a;
+int b;
+for (a = 1; a <= size; a++)
+{
+for (b = a; b < size; b++)
+{
+_putchar(' ');
+}
+for (b = 1; b <= a; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
