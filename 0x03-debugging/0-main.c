369a602de980fc0d1d7bf436d427f364495ccec7
Marvin Kurland <bugemarvin@outlook.com>
2022-04-18 14:51:21 +0300

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
 /**
 *main - tests function that prints if integer is positive or negative
 *Return: 0
 */
-
 int main(void)
 {
-
 int i;
 i = 0;
-
+if (i > 0)
 positive_or_negative(i);
-
-return (0);
-
+else if (i < 0)
+positive_or_negative(i);
+else if (!(n > 0) && !(n < 0))
+return (i);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 void positive_or_negative(int i);
 
