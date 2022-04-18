926ae04f2e0f5fb5b96ff6a6728b8d35d83b18f7
Marvin Kurland <bugemarvin@outlook.com>
2022-04-19 00:35:17 +0300

    task 9 fizzbuzz challenge

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
 * Description: main - FizzBuzz Challenge diviseble with 3,5 and 15
 * Return: 0 or 1 Always succes
 */
+
 int main(void)
 {
 int a;
 for (a = 1; a <= 100; a++)
 {
-if ((a % 3) == 0)
-{
-printf("Fizz");
-if ((a % 5) == 0)
-{
-printf("Buzz");
 if ((a % 15) == 0)
 {
 printf("FizzBuzz");
 }
+else if ((a % 3) == 0)
+{
+printf("Fizz");
 }
+else if ((a % 5) == 0)
+{
+printf("Buzz");
 }
 else
 {
 printf("%d", a);
 }
-printf(" ");
-if (a == 100)
-{
-printf(" ");
-}
+if (a < 100)
+putchar(' ');
 }
 printf("\n");
 return (0);
