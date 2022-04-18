1bb903507d40aaef07f066eb00a40b78a9a0aa98
Marvin Kurland <bugemarvin@outlook.com>
2022-04-18 23:46:07 +0300

    task 8 the size of square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,10 +1,26 @@
 #include "main.h"
 /**
  * print_square - Main entry point
+ * @size: prints the size of a square
  * Return: 0 or 1 On success.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void print_square(int size)
 {
-
+int a;
+int c;
+if (size > 0)
+{
+for (a = 0; a < size; a++)
+{
+for (c = 0; c < size; c++)
+{
+putchar(35);
+}
+putchar(10);
+}
+}
+else
+{
+putchar(10);
 }
