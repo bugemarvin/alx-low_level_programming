5e7c09a02d3d6cbc64dadbe94315244960f38886
Marvin Kurland <bugemarvin@outlook.com>
2022-04-18 16:53:40 +0300

    task 3

--- 0x03-debugging/tester.c
+++ 0x03-debugging/tester.c
@@ -73,9 +73,9 @@ int convert_day(int month, int day)
 
 void print_remaining_days(int month, int day, int year)
 {
-if (((year % 4) == 0 || (year % 400) == 0) && !((year % 100) == 0))
+if ((((year % 4) == 0) || ((year % 400) == 0)) && !((year % 100) == 0))
 {
-if (month >= 2 && day >= 60)
+if (month >= 3 && day >= 60)
 {
 day++;
 }
