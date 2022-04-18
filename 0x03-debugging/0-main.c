417884abc0502bd8594618ee60c9233fdce18099
Marvin Kurland <bugemarvin@outlook.com>
2022-04-18 14:58:14 +0300

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,19 @@
 #include "main.h"
+
 /**
 *main - tests function that prints if integer is positive or negative
 *Return: 0
 */
+
 int main(void)
 {
+
 int i;
-i = 0;
-if (i > 0)
+
+i = 0
+
 positive_or_negative(i);
-else if (i < 0)
-positive_or_negative(i);
-else if (!(n > 0) && !(n < 0))
-return (i);
+
+return (0);
+
 }
