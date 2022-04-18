12585efeb0073cbb8a02089b2398e663bdfe4699
Marvin Kurland <bugemarvin@outlook.com>
2022-04-18 23:25:20 +0300

    task 6 function to draw a line in terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,18 @@
 #include "main.h"
+
 /**
  * print_line - Main entry point
+ *@n: print out the number
  * Return: 0 or 1 On success.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 void print_line(int n)
 {
-
+int a;
+for (a = 0; a < n; a++)
+{
+putchar('_');
+}
+putchar('\n');
 }
--- 0x04-more_functions_nested_loops/testers.c
+++ 0x04-more_functions_nested_loops/testers.c
@@ -1,16 +1,13 @@
+/* C program to print a long int number
+using putchar() only*/
 #include <stdio.h>
-/**
-*Description: main - prints all digit of base 10 starting from 0, new line.
-*Return: 0 if successful
-*/
+
 int main(void)
 {
-int c;
-int b;
-for (c = 0; c < 10; c++)
-for ( b = 0; b <= 14; b++)
-if (b > 9)
-putchar((b / 10) + 0);
-putchar((b % 10) + 48);
-putchar(10);
+int a;
+for (a = 0; a < 100; a++)
+{
+putchar('_');
+}
+putchar('\n');
 }
