0133c6bfd3403966c97d0f0a88bfd17bbff18e5d
Marvin Kurland <bugemarvin@outlook.com>
2022-04-21 00:30:42 +0300

    task 1 swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * swap_int - main entry point.
+  * @a: prints the swaped integer b.
+  * @b: prints the swaped integer a.
+  */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
