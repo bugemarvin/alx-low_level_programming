0c26a6bd2656c930a9096930a023e43673d7353e
Marvin Kurland <bugemarvin@outlook.com>
2022-04-20 23:20:45 +0300

    initial readme and 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+*       prototypes of array and pointers
+*/
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
