0dd3b439a4d9df07416b490b21b75c05fdc2f544
Marvin Kurland <bugemarvin@outlook.com>
2022-04-21 02:37:05 +0300

    adding compilation for main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 /*
 *       prototypes of array and pointers
@@ -13,5 +14,4 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-
 #endif
