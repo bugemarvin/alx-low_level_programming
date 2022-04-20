aa34153baf4691ad68e5e313809ff7fe5c8858dd
Marvin Kurland <bugemarvin@outlook.com>
2022-04-20 23:21:33 +0300

    files update check

--- 0x04-more_functions_nested_loops/testers.c
+++ 0x04-more_functions_nested_loops/testers.c
@@ -1,13 +1,16 @@
-/* C program to print a long int number
-using putchar() only*/
 #include <stdio.h>
+
+/**
+ * Description: main - using putchar only function.
+ * Return: 0 or 1 Always Succes
+ **/
 
 int main(void)
 {
-int a;
-for (a = 0; a < 100; a++)
-{
-putchar('_');
-}
-putchar('\n');
+	int a;
+	for (a = 0; a < 100; a++)
+	{
+		putchar('_');
+	}
+	putchar('\n');
 }
