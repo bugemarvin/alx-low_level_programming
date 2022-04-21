0dedde53856acc7862340f0c66b84744f79603a9
marvin <bugemarvin@outlook.com>
2022-04-21 15:33:27 +0000

    task 102 combo of two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,24 +10,22 @@ int main(void)
 {
 	int a;
 	int b;
-		for (a = '0'; a <= '9'; a++)
+		for (a = 0; a < 100; a++)
 		{
-			for (b = a + 1; b <= '9'; b++)
+			for (b = 0; b < 100; b++)
 			{
-				if (a != b)
+				if (a < b)
 				{
-					if (a < '9' || b < '9')
-					{
-						putchar(a);
-						putchar(b);
-						a++;
-						b++;
-					}
-					if (a != '8')
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar((a / 10) + 48);
+					putchar((a % 10) + 48);
+					putchar(' ');
+					putchar((b / 10) + 48);
+					putchar((b % 10) + 48);
+				}
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
