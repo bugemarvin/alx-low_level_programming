5a3d219a328e3c27241883886b258f4dd84d0655
Marvin Kurland <bugemarvin@outlook.com>
2022-04-25 00:40:33 +0300

    function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,24 +16,24 @@ char *cap_string(char *s)
 				s[0] -= 32;
 			while (s[i] != '\0')
 			{
-				if (s[i] > 96 && s[i] < 123)
+			if (s[i] > 96 && s[i] < 123)
+			{
+				j = 0;
+				check = 0;
+				while (check == 0 && j < 13)
 				{
-					j = 0;
-					check = 0;
-						while (check == 0 && j < 13)
-						{
-							if (s[i - 1] == a[j])
-							{
-								check = 1;
-							}
-							j++;
-						}
-						if (check == 1)
-						{
-							s[i] -= 32;
-						}
+					if (s[i - 1] == a[j])
+					{
+						check = 1;
+					}
+					j++;
 				}
-				i++;
+				if (check == 1)
+				{
+					s[i] -= 32;
+				}
+			}
+			i++;
 			}
 			return (s);
 }
