865ab90abe3f26fca47002cfc3a130a4275338bd
Marvin Kurland <bugemarvin@outlook.com>
2022-04-25 01:14:03 +0300

    function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,56 +1,27 @@
 #include "main.h"
 
 /**
- *leet - encodes a string into 1337.
- *@s: string to encode.
- *Return: the encoded string.
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
  */
-
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i = 0, j = 0;
-	char array_leet[] = {'4', '3', '1', '0', '7'};
-	char array_up[] = {'A', 'E', 'L', 'O', 'T'};
-	char array_low[] = {'a', 'e', 'l', 'o', 't'};
+  int indx1 = 0, indx2;
+  char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (s[i] != '\0')
+  while (str[indx1])
+    {
+      for (indx2 = 0; indx2 <= 7; indx2++)
 	{
-		for (j = 0; j < 5; j++)
-		{
-			if (s[i] == array_low[j] || s[i] == array_up[j])
-				s[i] = array_leet[j];
-		}
-		i++;
+	  if (str[indx1] == leet[indx2] ||
+	      str[indx1] - 32 == leet[indx2])
+	    str[indx1] = indx2 + '0';
 	}
 
-	return (s);
-}#include "./main.h"
-#include <stdio.h>
-/**
- *leet - encodes a string into 1337.
- *@s: string to encode.
- *Return: the encoded string.
- */
+      indx1++;
+    }
 
-char *leet(char *s)
-{
-	int i;
-	int j;
-
-	i = 0;
-
-	char array_leet[] = {'4', '3', '1', '0', '7'};
-	char array_up[] = {'A', 'E', 'L', 'O', 'T'};
-	char array_low[] = {'a', 'e', 'l', 'o', 't'};
-
-	while (s[i] != '\0')
-	{
-		for (j = 0; j < 5; j++)
-		{
-			if (s[i] == array_low[j] || s[i] == array_up[j])
-				s[i] = array_leet[j];
-		}
-		i++;
-	}
-	return (s);
+  return (str);
 }
