6d1c78cdf7046f804afcf967faf8d901db151f39
Marvin Kurland <bugemarvin@outlook.com>
2022-04-25 00:35:44 +0300

    function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,43 +1,39 @@
 #include "main.h"
-
 /**
  * cap_string - capitalizes all words of a string.
  * @s: string to use.
- *
  * Return: string.
  */
 
 char *cap_string(char *s)
 {
-	int i; 
-        int j;
-        int check;
-                i = 1;
-char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
-
-	if (s[0] > 96 && s[0] < 123)
-		s[0] -= 32;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] > 96 && s[i] < 123)
-		{
-			j = 0;
-			check = 0;
-			while (check == 0 && j < 13)
+	int i;
+	int j;
+	int check;
+	char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
+		i = 1;
+			if (s[0] > 96 && s[0] < 123)
+				s[0] -= 32;
+			while (s[i] != '\0')
 			{
-				if (s[i - 1] == a[j])
+				if (s[i] > 96 && s[i] < 123)
 				{
-					check = 1;
+					j = 0;
+					check = 0;
+						while (check == 0 && j < 13)
+						{
+							if (s[i - 1] == a[j])
+							{
+								check = 1;
+							}
+							j++;
+						}
+						if (check == 1)
+						{
+							s[i] -= 32;
+						}
 				}
-				j++;
+				i++;
 			}
-			if (check == 1)
-			{
-				s[i] -= 32;
-			}
-		}
-		i++;
-	}
-return (s);
+			return (s);
 }
