1129cb40dcd49b76d47d7564f9ce8a938fc9ad7d
Marvin Kurland <bugemarvin@outlook.com>
2022-04-27 02:33:39 +0300

    task 8 working with diagnols

--- 0x07-pointers_arrays_strings/8-print_diagsuma.c
+++ 0x07-pointers_arrays_strings/8-print_diagsuma.c
@@ -12,12 +12,17 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int j;
 
-	int sum1 = 0;
-	int sum2 = 0;
+	unsigned int sum, sum1;
 
-	for (j = 0, i = size - 1; j < (size * size); j += size + 1, i += size - 1)
-		sum1 += a[j], sum2 += a[i];
-	printf("%d, %d\n", sum1, sum2);
+	sum = 0;
+	sum1 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum += a[(size * i) + i];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
+	}
+
+	printf("%d, %d\n", sum, sum1);
 }
