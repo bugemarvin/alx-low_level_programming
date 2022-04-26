399ae1fbaaa36848fa51e8ddbca8cc9a6e598a0b
Marvin Kurland <bugemarvin@outlook.com>
2022-04-26 22:18:59 +0300

    task 4 searching bytes of a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _strpbrk - main entry point for search.
+  * @s: first occurrence in a str.
+  * @accept: bytes of string.
+  *
+  * Return: 0 or 1 always succes
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int ser;
+	return (ser);
+}
