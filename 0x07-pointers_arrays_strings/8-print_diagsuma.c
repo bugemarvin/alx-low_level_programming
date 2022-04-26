3f9ce89aebd40bce529932d4f2b776460f0c2439
Marvin Kurland <bugemarvin@outlook.com>
2022-04-27 02:00:41 +0300

    task 8 working with diagnols

---
+++ 0x07-pointers_arrays_strings/8-print_diagsuma.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_diagsums - main entry point for diagonals.
+  * @a: array pointer.
+  * @size: len of the array.
+  *
+  * Return: 0 or 1 Always succes.
+  */
+
+void print_diagsums(int *a, int size)
+{
+	int b;
+
+	unsigned int tty;
+	unsigned int tty1;
+
+	tty = 0;
+	tty1 = 0;
+
+	for (b = 0; b < size; b++)
+	{
+		tty += a[(size * b) + b];
+		tty1 += a[(size * (b + 1)) - (b + 1)];
+	}
+	printf("%d, %d\n", tty, tty1);
+}
