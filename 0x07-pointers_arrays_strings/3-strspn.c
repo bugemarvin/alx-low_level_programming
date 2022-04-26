e5e97faa890730cc8b18674148a6b49ca9b2b465
Marvin Kurland <bugemarvin@outlook.com>
2022-04-26 21:02:25 +0300

    task 3 len of substr

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _strspn - main entry point for len substring.
+  * @s: first pointer of the string
+  * @accept: second pointer for the bytes.
+  *
+  * Return: 0 or 1 Alwyas succes.
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (s);
+}
