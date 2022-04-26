e51c8ac345cf744311457113e1213c748451e237
marvin <bugemarvin@outlook.com>
2022-04-26 15:15:21 +0000

    test task 0 adding bytes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,14 +3,14 @@
 /**
   * _memset - main entry point.
   * @s: first string is a pointer.
-  * @b: second string.
-  * @n: prints a number.
+  * @b: second string as a constant.
+  * @n: prints the number of bytes.
   *
-  * Returns: s always succes.
+  * Returns: 0 or 1 always succes.
   */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	memset(s + 0, b, n);
-	return(s);
+	return (s);
 }
