fe88993adad28c90638200fe273bc4f967ad00bd
marvin <bugemarvin@outlook.com>
2022-04-26 16:03:04 +0000

    task 2 locates a string in a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _strchr - main entry point.
+  * @s: first string is a pointer.
+  * @c: second string.
+  *
+  * Return: 0 or 1 always on succes.
+  */
+
+char *_strchr(char *s, char c)
+{
+	strchrnul(s + 0, c);
+	return (s);
+}
