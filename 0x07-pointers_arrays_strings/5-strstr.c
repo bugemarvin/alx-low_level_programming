c7f66b2875768bfebd89f3cdb43b494186bb7f93
Marvin Kurland <bugemarvin@outlook.com>
2022-04-27 00:18:41 +0300

    task 5 locates a substring.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _strstr - main entry point to locate substirng.
+  * @haystack: first occurrence of the substring.
+  * @needle: this is the string.
+  *
+  * Return: 0 or 1 Always succes.
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (haystack != '\0')
+	{
+		return (strstr(haystack, needle));
+	}
+	return ('\0');
+}
