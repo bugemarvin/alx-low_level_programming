56c799f4ec5bc1e6f6e5bb7797234853af37ca40
marvin <bugemarvin@outlook.com>
2022-04-26 15:39:41 +0000

    task 2 coping memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _memcpy - main entry point for copy.
+  * @dest: the first string of a pointer.
+  * @src: the second string of a pointer.
+  * @n: number of bytes for the memory.
+  *
+  * Return: 0 or 1 Always on succes.
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest + 0; src; n);
+	return (dest);
+}
