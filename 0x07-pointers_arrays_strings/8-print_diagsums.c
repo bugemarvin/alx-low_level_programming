9b15c15f6e2962f3e98702905f4e9cd17ae862ea
marvin <bugemarvin@outlook.com>
2022-04-27 06:21:58 +0000

    sum of diagonals square matrixs

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * print_diagsums - main entry point for diagonals.
+  * @a: array pointer.
+  * @size: len of the array.
+  *
+  * Return: 0 or 1 Always succes.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int b;
+	int sum = 0;
+	int sum1 = 0;
+
+	for (b = 0; b < size; b++)
+	{
+		sum += a[b];
+		a += size;
+	}
+
+	a -= size;
+
+	for (b = 0; b < size; b++)
+	{
+		sum1 += a[index];
+		a -= size;
+	}
+	printf("%d, %d\n", sum, sum1);
+}
