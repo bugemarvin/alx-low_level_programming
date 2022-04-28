016e3b123fce9e07678090ab10fcc1e4969ba092
Marvin <bugemarvin@outlook.com>
2022-04-28 18:45:28 +0000

    initial task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _print_rev_recursion - main entry point.
+  * @s: pointer string to be reversed.
+  *
+  * Return: Always (0) on succes.
+  */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+	}
+	else
+	{
+	}
+}
