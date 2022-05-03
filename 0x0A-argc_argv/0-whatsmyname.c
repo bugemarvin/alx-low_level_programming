5b0235b581f0fe39791d13ddcc39fb31f2270c3b
Marvin Kurland <bugemarvin@outlook.com>
2022-05-03 14:43:41 +0300

    initial main file & _putchar proto

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,4 @@
+#include "main.h"
+
+/**
+  * _filename - As the main entry point
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H_INCLUDE
+#define MAIN_H_INCLUDE
+
+/**
+  * Author: Marvin Kurland
+  * About: Recursion in C language.
+  *
+  */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/*
+*	main - entry for prototypes.
+*	return: (0) Always succes.
+*/
+
+int _putchar(char c);
+
+
+#endif
