ecd1e645f57354dde5bb60eb75b93a1188e7e79f
Marvin Kurland <bugemarvin@outlook.com>
2022-05-03 15:39:48 +0300

    task 0 printing the file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,14 +8,14 @@
   * Return: Always (0) on succes.
   */
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
-		int z = strtol(argv[i],NULL,10);
-		if (argc < 1)
-			putchar(z);
+		if (argc <= 1)
+			printf("%s\n", argv[i]);
 	}
 	return (0);
 }
