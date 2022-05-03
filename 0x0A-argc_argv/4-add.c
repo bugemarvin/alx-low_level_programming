30f9abb981e753a666cc2dfdaa249f0dc4486763
Marvin Kurland <bugemarvin@outlook.com>
2022-05-04 02:09:26 +0300

    task 4 program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -32,11 +32,13 @@ int main(int argc, char *argv[])
 		c = strtol(argv[a], NULL, 10);
 		sum += c;
 	}
-	if (argc >= 2)
+	if (argc <= )
+	{
+		printf("0\n");
+	}
+	else
 	{
 		printf("%d\n", sum);
 	}
-	printf("0\n");
-
 	return (0);
 }
