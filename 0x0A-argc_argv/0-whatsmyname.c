fbc1d2a35c7eb7513739fd4c3211536dc5e509f3
Marvin Kurland <bugemarvin@outlook.com>
2022-05-03 14:49:26 +0300

    task 0 printing file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
-  * _filename - As the main entry point
+  * _filename - As the main entry point.
+  * @argc: argument count.
+  * @argv: argument value.
+  *
+  * Return: Always (0) on succes.
+  */
+
+int _filename(int argc, char* argv)
+{
+	int a;
+	for (a = 0; a < argc; a++)
+		if (argc < 1)
+			printf("%s", argv[a]);
+
+	return (0);
+}
