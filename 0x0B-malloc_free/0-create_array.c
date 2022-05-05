71bddff58cf084c44917edde7181375eba3600f1
Marvin Kurland <bugemarvin@outlook.com>
2022-05-06 01:09:59 +0300

    task 0 initialize chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * create_array - main entry for chars stdout in malloc.
+  * @size: size of memory allocation for array.
+  * @c: string of an array.
+  *
+  * Return: Always (0) for succes.
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p = (char *)malloc(size * sizeof(char);
+	int a;
+
+	for (a = 0; a < size; a++)
+	{
+		p[a] = c;
+	}
+
+	if (size > 0)
+	{
+		return (p);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
