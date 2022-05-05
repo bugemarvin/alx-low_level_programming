f96176fff9af0434bf29b7d9a1897abcbe25c9e5
Marvin Kurland <bugemarvin@outlook.com>
2022-05-05 22:41:50 +0300

    initial main file and putchar function.

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/*
+ * Author: Marvin Kurland
+ * About: Recursion in C language.
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/*
+ *	main - entry for prototypes.
+ *	return: (0) Always succes.
+ */
+
+int _putchar(char c);
+
+#endif
