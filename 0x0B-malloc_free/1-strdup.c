20e800e17081a89c5e8e7204cd4d605518c6aa53
marvin <bugemarvin@outlook.com>
2022-05-06 13:51:37 +0000

    returns a pointer to a newly allocated space in memory and copys

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,34 +3,38 @@
 #include <unistd.h>
 
 /**
-  * *_strdup(char *str) - main entry allocated space memory.
+  * _strdup(char *str) - main entry allocated space memory.
   * @str: pointer for a new space of memory.
   *
   * Returns: Always (0) on succes.
-  * Returns: NULL if empty as well as insufficent memory.
+  * Returns: -1 NULL if empty as well as insufficent memory.
   */
 
 char *_strdup(char *str)
 {
 	int a;
-	char *str2;
+	char *p;
+	int size = 100;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	str2 = (char *)malloc(*(str - 1) * sizeof(char));
+	p = (char *)malloc(size * sizeof(char));
 
-	if (str2 == NULL)
+	if (p == NULL)
 	{
 		return (NULL);
 	}
 
-	for (a = 0; a < '\0'; a++)
+	for (a = 0; a < size; a++)
 	{
-			str2[a] = str[a];
+		if (size > 0)
+		{
+			p[a] = str[a];
+		}
 	}
 
-	return (str2);
+	return (p);
 }
