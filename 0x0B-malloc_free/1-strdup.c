99e15fa4a8396e9fb7b5d462c4fb29dd459a2e81
marvin <bugemarvin@outlook.com>
2022-05-06 12:32:43 +0000

    task 1 check

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,10 +12,29 @@
 
 char *_strdup(char *str)
 {
+	int a;
+	char *str2;
+	int size = 10;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	
-	return (str);
+
+	str2 = (char *)malloc(size * sizeof(char));
+
+	if (str2 == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a < size; a++)
+	{
+		if (size > 0)
+		{
+			str2[a] = str[a];
+		}
+	}
+
+	return (str2);
 }
