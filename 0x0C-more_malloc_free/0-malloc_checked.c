4352f7dbf04296f878a7f0db5482b616522a7113
marvin <bugemarvin@outlook.com>
2022-05-10 13:29:38 +0000

    function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * malloc_checked - main entry for allocating malloc.
+  * @b: int for casting malloc memory.
+  *
+  * Return: Always (0) for succes.
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	int a;
+	unsigned int *p = &b;
+	int size;
+
+	for (a = 0; p[a] != '\0'; a++)
+	{
+		size++;
+	}
+
+	p = (unsigned int *)malloc(size  * sizeof(unsigned int));
+
+	if (p == NULL)
+	{
+		return (0);
+	}
+		return (p);
+}
