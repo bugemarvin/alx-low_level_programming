88d63729e26f330f15cdf2673b9524f0dfab857c
Marvin Kurland <bugemarvin@outlook.com>
2022-05-12 00:02:00 +0300

    function that creates an array of integers.

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * _calloc - main entry for allocating mem to array in C.
+  * @nmemb: member of the array.
+  * @size: size of the array.
+  *
+  * Return: Always (NULL) malloc  fails, (0) for succes)
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int a, *p;
+
+	a = nmemb * size;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(a);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	p[a] = '\0';
+	return(p);
+}
