22e551fcef8309b24b93babaaf58f7b83bb31d67
Marvin Kurland <bugemarvin@outlook.com>
2022-05-12 19:04:44 +0300

    program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ME\
+	printf("%s\n", __FILE__)
+
+/**
+ * main - return the file name.
+ *
+ * Return: Always (0) for succes.
+ */
+
+int main(void)
+{
+	ME;
+	return (0);
+}
