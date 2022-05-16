c99f7fd3217b5d4fd1b6d299d8029f6a033493d7
Marvin Kurland <bugemarvin@outlook.com>
2022-05-17 02:33:44 +0300

    functions update && calc header incusion removal

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+  * op_add - function to add two integers.
+  * @a: first integer.
+  * @b: second integer.
+  *
+  * Return: Always (0)  for succes.
+  */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - function for subtraction.
+  * @a: first integer.
+  * @b: Second integer.
+  *
+  * Return: Always (0) for succes.
+  */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - function for multiplication.
+  * @a: first integer.
+  * @b: Second integer.
+  *
+  * Return: Always (0) for succes.
+  */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - function for division.
+  * @a: first integer.
+  * @b: second intger.
+  *
+  * Return: Always (0) for succes.
+  */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - function for modular/remider.
+  * @a: first interger.
+  * @b: second interger.
+  *
+  * Return: Always (0) for succes
+  */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
