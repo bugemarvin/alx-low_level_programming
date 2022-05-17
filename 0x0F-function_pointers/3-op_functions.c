ead52d2abd8b089d0cf5bcd3f80de8d8be96b2d2
Marvin Kurland <bugemarvin@outlook.com>
2022-05-17 04:15:50 +0300

    program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 /**
   * op_add - function to add two integers.
@@ -51,11 +52,12 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	if (b = 0)
+	if (!b)
 	{
-		printf("Error\n"),
-			exit(100);
-	return (a / b);
+		return (a / b);
+	}
+	printf("Error\n"),
+		exit(100);
 }
 
 /**
@@ -68,10 +70,10 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	if (b = 0)
+	if (!b)
 	{
-		printf("Error\n"),
-			exit(100);
+		return (a % b);
 	}
-	return (a % b);
+	printf("Error\n"),
+		exit(100);
 }
