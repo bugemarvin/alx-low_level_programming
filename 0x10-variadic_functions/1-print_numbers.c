cc1d31dcbd1bfa2cb8211f61ca29b97a5914e6b6
Marvin Kurland <bugemarvin@outlook.com>
2022-05-17 23:57:51 +0300

    function that prints numbers.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		if (a != (n - 1) && separator != NULL)
 		{
-			printf("%s ", separator);
+			printf("%s", separator);
 		}
 	}
 	putchar('\n');
