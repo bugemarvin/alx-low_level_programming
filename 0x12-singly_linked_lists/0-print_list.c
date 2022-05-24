035bd8280cedd59ccc479352920d537ba6a5ae80
marvin <bugemarvin@outlook.com>
2022-05-24 16:16:51 +0000

    heading home

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,13 @@
 #include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
 
 size_t print_list(const list_t *h)
 {
-	if (h == NULL)
+	if (h != NULL)
 	{
-		printf("(nil)");
+		printf("[%u] %s\n", h->len, h->str);
 	}
+	else
+		printf("[%u] %s\n", (*h).len = 0, (*h).str = "(nil)");
 }
