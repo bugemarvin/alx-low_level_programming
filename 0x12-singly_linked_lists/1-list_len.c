d60e85c4ad5c8ee1b3ed472e37c5bbeb15a484cb
Marvin Kurland <bugemarvin@outlook.com>
2022-05-24 22:53:17 +0300

    function that returns the number of elements in a linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,10 +10,10 @@
 size_t list_len(const list_t *h)
 {
 	size_t  a;
-	for (a = 0; h; a++)
-	{
-		if (h->str != NULL)
-			h = h->next;
-	}
+		for (a = 0; h; a++)
+		{
+			if (h->str != NULL)
+				h = h->next;
+		}
 	return (a);
 }
