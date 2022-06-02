f0bffd8196ac80f525b13c08f0cf51f860e40af7
Marvin Kurland <bugemarvin@outlook.com>
2022-06-03 01:29:27 +0300

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -40,7 +40,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	creates_f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (creates_f == -1 || size_len == -1)
+	if (creates_f == -1 && size_len == -1)
 	{
 		return (-1);
 	}
