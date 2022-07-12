8b0a58f8738872d0d193e61b9c927541ce48bba9
Marvin Kurland <bugemarvin@outlook.com>
2022-07-12 09:17:22 +0300

    Dynamic linking

--- 0x18-dynamic_libraries/puts.c
+++ 0x18-dynamic_libraries/puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
-* Description: main-Creating a C program that outputs by use of function puts
-* Return: 0 if successful
-*/
+ * Description: main-Creating a C program that outputs by use of function puts
+ * Return: 0 if successful
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
