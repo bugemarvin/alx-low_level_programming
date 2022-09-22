fab75a4bddba78dcfd9af4812d6744d47b5a30c5
Marvin Kurland <bugemarvin@outlook.com>
2022-09-22 19:49:56 +0300

    islan perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@ function def island_perimeter(grid):
 that returns the perimeter of the island described in grid:
 '''
 
+
 def island_perimeter(grid):
     '''function representation of the grid.
     '''
