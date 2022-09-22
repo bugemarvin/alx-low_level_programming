756322f4975c92df2e6f562d69c7705b70aabb68
Marvin Kurland <bugemarvin@outlook.com>
2022-09-22 17:21:21 +0300

    islan perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,6 +15,8 @@ def island_perimeter(grid):
         for c in range(width):
             if grid[b][c] == 1:
                 size += 1
-                if ((c > 0 and grid[b][c - 1]) or (b > 0 and grid[b - 1][c])) == 1:
+                if (c > 0 and grid[b][c - 1]) == 1:
+                    border += 1
+                if (b > 0 and grid[b - 1][c]) == 1:
                     border += 1
     return size * 4 - border * 2
