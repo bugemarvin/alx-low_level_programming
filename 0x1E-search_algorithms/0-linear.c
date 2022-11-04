ee0cc72cd0e2a70e01642935a639bff1d8ef09e5
Marvin Kurland <bugemarvin@outlook.com>
2022-11-04 15:37:34 +0300

    linear function to return value

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,12 +1,12 @@
 #include "search_algos.h"
 
 /**
- * liner_search - function to search values in an array of ints
- * @array: pointer of an array of elements
- * @size: the length of an array
- * @value: data of the array
+ * linear_search - function to search values in an array of ints.
+ * @array: pointer of an array of elements.
+ * @size: the length of an array.
+ * @value: data of the array.
  *
- * Return - Always 0
+ * Return: Always -1 if value does not match.
  */
 
 int linear_search(int *array, size_t size, int value)
